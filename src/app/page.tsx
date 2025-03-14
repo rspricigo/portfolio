e99e3f2daf8f9a7a2a83b519019cf9c80d1cb82a
Reginaldo Spricigo <reginaldo.spricigo@weinnovate.com.br>
2025-03-14 13:52:22 -0300

    Add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -32,6 +33,7 @@ export default function Home() {
           </div>
         </main>
       </div>
+      <Footer />
     </>
   );
 }
